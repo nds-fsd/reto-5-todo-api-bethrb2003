b9457adb3d163177b31a366124adf67de157d7db
bethrb2003 <beth.remeseiro.2003@gmail.com>
2023-11-23 20:43:50 +0100

    api terminada

--- src/data/index.js
+++ src/data/index.js
@@ -1,14 +1,52 @@
-exports.todos = [{
-  id: 0,
-  text: "Hacer la cama",
-  fecha: new Date("2022-05-21"),
-  done: false
+exports.tasks = [{
+  "id": 1,
+  "name": "Barrer",
+  "complete": "false",
 },
-{ 
-  id: 1,
-  text: "Ir al super",
-  fecha: new Date("2022-05-14"),
-  done: true
+{
+  "id": 2,
+  "name": "Fregar",
+  "complete": "false",
+},
+{
+  "id": 3,
+  "name": "Hacer la comida",
+  "complete": "false",
+},
+{
+  "id": 4,
+  "name": "Poner lavadora",
+  "complete": "false",
+},
+{
+  "id": 5,
+  "name": "Poner secadora",
+  "complete": "false",
+},
+{
+  "id": 6,
+  "name": "Limpiar el baño",
+  "complete": "false",
+},
+{
+  "id": 7,
+  "name": "Hacer deberes",
+  "complete": "false",
+},
+{
+  "id": 8,
+  "name": "Hacer la cena",
+  "complete": "false",
+},
+{
+  "id": 9,
+  "name": "Lavar los platos",
+  "complete": "false",
+},
+{
+  "id": 10,
+  "name": "Recoger la habitación",
+  "complete": "false",
 }];
 
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
 const todoRouter = require('./routers/todo');
+const { networkInterfaces } = require('os');
 
-
-//Le decimos a nuestra app, que vamos recibir peticiones donde el Body contiene texto en formato JSON.
 app.use(express.json());
 
-//Le decimos a nuestra app, que "utilize" el router de todos. Esto es equivalente a haber definido todos nuestros endpoints directamente sobre el objeto app como vimos en clase.
 app.use(todoRouter);
 
-//a partir de este punto y gracias a la linea escrita mas arriba, si llega alguna peticion que empieze por /todo, está se redirige hacia todoRouter.
+app.use((req, res, next) => {
+    console.log(`${req.method}: ${req.path}`);
+    next();
+})
 
-
-
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server is up and running in port 3000");
